79dcd2f0aef8db341c8ec3c769049b3b0a4c3adc
Peter Shrader <pshrader@bsd220.org>
2022-01-28 10:35:11 -0600

    edit

--- Assignment Notebook/Assignment View.swift
+++ Assignment Notebook/Assignment View.swift
@@ -27,7 +27,7 @@ struct Assignment_View: View {
                 TextField("Description", text: $description).foregroundColor(.blue)
                 DatePicker("Due Date", selection: $dueDate, displayedComponents: .date)
             }
-            .navigationBarTitle("Add New To-Do Item", displayMode: .inline)
+            .navigationBarTitle("Add New Assignment", displayMode: .inline)
             .navigationBarItems(trailing: Button("Save") {
                 if course.count > 0 && description.count > 0 {
                     let item = AssignmentItem(id: UUID(), course: course, description: description, dueDate: dueDate)
